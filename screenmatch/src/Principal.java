5cb867428cc5feaaea30933616e2fba29edeeb71
Mateus Raffaelli <mattewscastro@gmail.com>
2026-03-19 14:50:45 -0300

    Adicionando lista de filmes com ArrayList

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -4,6 +4,8 @@ import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal{
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
@@ -47,5 +49,21 @@ public class Principal{
         episodio.setSerie(lost);
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
+
+        var filmeMateus = new Filme();
+        filmeMateus.setDuracaoEmMinutos(200);
+        filmeMateus.setNome("Cidade de Deus");
+        filmeMateus.setAnoDeLancamento(2002);
+        filmeMateus.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeMateus);
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+        System.out.println("Tamanho da lista: " + listaDeFilmes.size());
+        System.out.println("Primeiro filme: " + listaDeFilmes.get(0).getNome());
+        System.out.println(listaDeFilmes);
+        System.out.println("toString do filme - " + listaDeFilmes.get(0).toString());
+
     }
 }
