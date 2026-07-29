cfd545b8049b6a944b17f321c6d1a954fbd03463
Mateus Raffaelli <mattewscastro@gmail.com>
2026-07-29 08:29:14 -0300

    Melhorando funcionalidades do projeto

--- screenmatch/src/br/com/alura/screenmatch/modelos/Titulo.java
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,5 +1,7 @@
 package br.com.alura.screenmatch.modelos;
 
+import br.com.alura.screenmatch.excecao.ErroDeConversaoDeAnoException;
+
 public class Titulo implements Comparable<Titulo>{
     private String nome;
     private int anoDeLancamento;
@@ -11,6 +13,17 @@ public class Titulo implements Comparable<Titulo>{
     public Titulo(String nome, int anoDeLancamento){
         this.nome = nome;
         this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public Titulo(TituloOmdb meuTituloOmdb) {
+        this.nome = meuTituloOmdb.title();
+
+        if (meuTituloOmdb.year().length() > 4){
+            throw new ErroDeConversaoDeAnoException("Não consegui converter o ano porque tem mais de 4 caracteres");
+
+        }
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0,2));
     }
 
     public int getTotalDeAvaliacoes(){
@@ -68,5 +81,13 @@ public class Titulo implements Comparable<Titulo>{
     @Override
     public int compareTo(Titulo outroTitulo) {
         return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "(Nome: " + nome +
+                ", Ano de Lancamento: " + anoDeLancamento +
+                ", Duração: " + duracaoEmMinutos +
+                ") ";
     }
 }
